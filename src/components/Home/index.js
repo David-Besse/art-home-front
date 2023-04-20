e22812d5c38c5aa25cb7ae82e9dc997597c63b17
David-Besse <dahodprod@gmail.com>
2023-04-20 09:45:56 +0200

    move loader directory and CSS modifications

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,7 +3,7 @@ import { useSelector } from 'react-redux';
 import Header from 'src/components/Header';
 import Page from 'src/components/Page';
 import Footer from 'src/components/Footer';
-import Loading from 'src/components/App/Loading';
+import Loading from './Loading';
 import HomeCarousel from './HomeCarousel';
 
 // TODO Review the police size depending on the screen size
