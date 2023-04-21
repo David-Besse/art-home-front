309b5f3292f40618cd34cb635fee8a2d1f5a85ed
Maiichuu6 <maiite.soulard@gmail.com>
2023-04-21 14:56:04 +0200

    restructuring css

--- src/components/OneExhibition/Pictures/index.js
+++ src/components/OneExhibition/Pictures/index.js
@@ -23,7 +23,7 @@ const Pictures = () => {
         <Card.Img className="avatar" variant="top" src={artist.avatar} alt={artist.slug} />
         <Card.Body className="body-artist">
           <Card.Title className="nickname">{artist.nickname}</Card.Title>
-          <Card.Text className="presentation">{artist.firstname} {artist.lastname}</Card.Text>
+          <Card.Text className="realname">{artist.firstname} {artist.lastname}</Card.Text>
           <Card.Text className="presentation">{artist.presentation}</Card.Text>
         </Card.Body>
       </Card>
