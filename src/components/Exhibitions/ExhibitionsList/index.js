b9b12329c47ac846a99982e0d771ce0a5865312a
Maiichuu6 <maiite.soulard@gmail.com>
2023-04-21 18:48:12 +0200

    starting css for the presentation of all exhibitions

--- src/components/Exhibitions/ExhibitionsList/index.js
+++ src/components/Exhibitions/ExhibitionsList/index.js
@@ -9,22 +9,21 @@ const ExhibitionsList = () => {
   const { list } = useSelector((state) => state.exhibitions);
 
   return (
-    <div className="exhibition">
+    <div className="all-exhibitions">
+
+      <h1 className="page-title">Nos expositions</h1>
 
       {/* Showcase of all the picture included in the exhibiton */}
-      <Row xs={1} md={1} lg={2} className="g-4">
+      <Row xs={1} md={2} lg={3} className="g-4">
         {list.map((exhibition) => (
           <Col className="col-picture" key={exhibition.slug}>
 
-            <Card className="bg-dark text-white">
-              <Card.Img src="holder.js/100px270" alt="Card image" />
-              <Card.ImgOverlay>
-                <Card.Title>Card title</Card.Title>
-                <Card.Text>
-                  This is a wider card with supporting text below as a natural lead-in
-                  to additional content. This content is a little bit longer.
-                </Card.Text>
-                <Card.Text>Last updated 3 mins ago</Card.Text>
+            <Card className="text-white card-info">
+              <Card.Img className="image-info" src={exhibition.picture} alt="Card image" />
+              <Card.ImgOverlay className="exhibition-info">
+                <Card.Title className="title-info">{exhibition.title}</Card.Title>
+                <Card.Text className="description-info">Artistname</Card.Text>
+                <Card.Text className="description-info">{exhibition.description}</Card.Text>
               </Card.ImgOverlay>
             </Card>
           </Col>
@@ -39,11 +38,11 @@ export default ExhibitionsList;
 
 /*
 <Card className="card-picture" >
-              <Card.Img className="avatar" variant="top" src={exhibition.artist.avatar} alt={exhibition.artist.slug} />
-              <Card.Img className="image-picture" src={picture.picture} alt={exhibition.slug} />
-              <Card.Body className="body-picture">
-                <Card.Title className="title">{title}</Card.Title>
-                <Card.Title className="nickname">{artist.nickname}</Card.Title>
-                <Card.Text className="description-picture">{exhibition.description}</Card.Text>
-              </Card.Body>
-          </Card> */
+  <Card.Img className="avatar" variant="top" src={exhibition.artist.avatar} alt={exhibition.artist.slug} />
+  <Card.Img className="image-picture" src={picture.picture} alt={exhibition.slug} />
+    <Card.Body className="body-picture">
+      <Card.Title className="title">{title}</Card.Title>
+      <Card.Title className="nickname">{artist.nickname}</Card.Title>
+      <Card.Text className="description-picture">{exhibition.description}</Card.Text>
+    </Card.Body>
+</Card> */
