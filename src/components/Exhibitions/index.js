e9317261d4fd2752409c38684e165e8ad175597f
David-Besse <dahodprod@gmail.com>
2023-04-22 15:25:00 +0200

    add loader component to exhibitions page

--- src/components/Exhibitions/index.js
+++ src/components/Exhibitions/index.js
@@ -1,16 +1,25 @@
 import Header from 'src/components/Header';
 import Page from 'src/components/Page';
 import Footer from 'src/components/Footer';
+import { useSelector } from 'react-redux';
 import ExhibitionsList from './ExhibitionsList';
+import Loading from '../Home/Loading';
 
-const Exhibitions = () => (
-  <>
-    <Header />
-    <Page>
-      <ExhibitionsList />
-    </Page>
-    <Footer />
-  </>
-);
+const Exhibitions = () => {
+  const { isExhibitionsLoaded } = useSelector((state) => state.exhibitions);
+
+  return (
+    <>
+      <Header />
+      <Page>
+        { !isExhibitionsLoaded
+        && <Loading /> }
+        { isExhibitionsLoaded
+        && <ExhibitionsList />}
+      </Page>
+      <Footer />
+    </>
+  );
+};
 
 export default Exhibitions;
