2cc65140c95e14329465128bc26358e97d7339ed
Maiichuu6 <maiite.soulard@gmail.com>
2023-04-25 11:03:16 +0200

    preparing the modal for the artist

--- src/components/OneExhibition/ArtistPresentation/index.js
+++ src/components/OneExhibition/ArtistPresentation/index.js
@@ -2,6 +2,8 @@ import Card from 'react-bootstrap/Card';
 import { useParams } from 'react-router-dom';
 import { findExhibition } from 'src/selectors/pictures';
 import { useSelector } from 'react-redux';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 
 const ArtistPresentation = () => {
   const { slug } = useParams();
@@ -20,3 +22,51 @@ const ArtistPresentation = () => {
 };
 
 export default ArtistPresentation;
+
+
+/* 
+
+
+function MyVerticallyCenteredModal(props) {
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          {artist.nickname}
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <img {artist.avatar} alt={artist.slug}
+        <h3>{artist.firstname} {artist.lastname}</h3>
+        <p>{artist.presentation}</p>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={props.onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
+
+function App() {
+  const [modalShow, setModalShow] = React.useState(false);
+
+  return (
+    <>
+      <Button variant="primary" onClick={() => setModalShow(true)}>
+        Launch vertically centered modal
+      </Button>
+
+      <MyVerticallyCenteredModal
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+      />
+    </>
+  );
+}
+
+render(<App />); */
