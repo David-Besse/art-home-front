e55dbfbb427414a01ff1ae090df784e7173b7eac
Maiichuu6 <maiite.soulard@gmail.com>
2023-04-26 14:38:26 +0200

    trying error 404

--- src/components/Error/index.js
+++ src/components/Error/index.js
@@ -1,14 +1,16 @@
 import Page from 'src/components/Page';
 import { LinkContainer } from 'react-router-bootstrap';
 import Button from 'react-bootstrap/Button';
+import Error404 from './Error404';
 
 const Error = () => (
   <Page>
     <div className="d-flex flex-column align-items-center justify-content-center">
+      <Error404 />
       <h1>Erreur</h1>
       <p>Nous sommes désolé, Une erreur s'est produite.</p>
       <LinkContainer to="/">
-        <Button>Retour vers la page d'accueil</Button>
+        <Button className="button">Retour vers la page d'accueil</Button>
       </LinkContainer>
     </div>
   </Page>
