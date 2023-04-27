c550d5bae4a94489d9ffc32a1c88fb66a7f12441
Maiichuu6 <maiite.soulard@gmail.com>
2023-04-27 19:55:48 +0200

    changing the card organisation

--- src/components/Exhibitions/ExhibitionsList/index.js
+++ src/components/Exhibitions/ExhibitionsList/index.js
@@ -1,6 +1,4 @@
 import Card from 'react-bootstrap/Card';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
 import { useSelector } from 'react-redux';
 import { LinkContainer } from 'react-router-bootstrap';
 
@@ -10,28 +8,24 @@ const ExhibitionsList = () => {
   const { list } = useSelector((state) => state.exhibitions);
 
   return (
-    <div className="all-exhibitions mx-4 d-flex flex-column">
+    <div className="all-exhibitions d-flex flex-column">
 
-      <h1 className="page-title my-3">Nos expositions</h1>
+      <h1 className="page-title my-3">NOS EXPOSITIONS</h1>
 
       {/* Showcase of all the picture included in the exhibiton */}
-      <div className="mb-4">
-        <Row xs={1} md={2} lg={3} className="g-4">
-          {list.map((exhibition) => (
-            <Col className="col-picture d-flex justify-content-center p-0" key={exhibition.slug}>
-              <Card className="text-white card-info">
-                <LinkContainer to={`/expositions/${exhibition.slug}`}>
-                  <Card.Img className="image-info" src={exhibition.picture} alt="Card image" />
-                </LinkContainer>
-                <Card.ImgOverlay className="exhibition-info">
-                  <Card.Title className="title-info">{exhibition.title}</Card.Title>
-                  <Card.Text className="description-info">Artistname</Card.Text>
-                  <Card.Text className="description-info">{exhibition.description}</Card.Text>
-                </Card.ImgOverlay>
-              </Card>
-            </Col>
-          ))}
-        </Row>
+      <div className="mb-4 d-flex flex-wrap justify-content-around exhibition-container">
+        {list.map((exhibition) => (
+          <LinkContainer to={`/expositions/${exhibition.slug}`} key={exhibition.slug}>
+            <Card className="text-white card-info">
+              <Card.Img className="image-info" src={exhibition.picture} alt={exhibition.slug} />
+              <Card.Body className="exhibition-info">
+                <Card.Title className="title-info">{exhibition.title}</Card.Title>
+                <Card.Text className="nickname-info">{exhibition.nickname}</Card.Text>
+                <Card.Text className="description-info">{exhibition.description}</Card.Text>
+              </Card.Body>
+            </Card>
+          </LinkContainer>
+        ))}
       </div>
 
     </div>
