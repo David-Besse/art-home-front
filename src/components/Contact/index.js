785ae9adfeb69f3f89af203dc5f4b0ee4506e179
David-Besse <dahodprod@gmail.com>
2023-05-02 21:37:40 +0200

    add CSS to disclaimer page and contact page

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -2,11 +2,39 @@ import Header from 'src/components/Header';
 import Page from 'src/components/Page';
 import Footer from 'src/components/Footer';
 
+import './styles.scss';
+
 const Contact = () => (
   <>
     <Header />
     <Page>
-      <div>PAGE CONTACT</div>
+      <div className="contact-page">
+        <section className="contact-infos">
+          {/* <h3>Nous contacter</h3> */}
+          <p className="contact-intro">
+            Nous sommes toujours ravis d'avoir de vos nouvelles. Si vous avez
+            des questions, des commentaires ou des suggestions, n'hésitez pas à
+            nous contacter !
+          </p>
+          <h4>Par email:</h4>
+          <p>
+            <strong>contact-art_home@gmail.com</strong><br />
+            Nous ferons de notre mieux pour répondre à
+            votre mail dans les plus brefs délais.
+          </p>
+          <h4>Par téléphone:</h4>
+          <p>
+            +33 01 23 45 67 89 <br />
+            Notre service clientèle est
+            disponible du lundi au vendredi de 9h à 18h. (hors jours fériés)
+          </p>
+          <h4>Par courrier:</h4>
+          <p>Art@home <br />
+            123 boulevard des oeuvres<br />
+            75000 Paris France
+          </p>
+        </section>
+      </div>
     </Page>
     <Footer />
   </>
--- src/components/Disclaimer/index.js
+++ src/components/Disclaimer/index.js
@@ -2,48 +2,59 @@ import Header from 'src/components/Header';
 import Page from 'src/components/Page';
 import Footer from 'src/components/Footer';
 
+import './styles.scss';
+
 const Disclaimer = () => (
   <>
     <Header />
     <Page>
-      <div className="p-2 text-center m-5">
+      <section className="disclaimer-page">
+
+        <h3>Objet des CGU</h3>
         <p>
-          Objet des CGU
-          Les présentes CGU ont pour objet de définir les conditions d'utilisation de votre site internet [Art@home] par les utilisateurs.
+          Les présentes CGU ont pour objet de définir les conditions d'utilisation de votre site internet <strong>Art@home</strong> par les utilisateurs.
         </p>
 
-        <p>Acceptation des CGU
+        <h3>Acceptation des CGU</h3>
+        <p>
           L'utilisation du site implique l'acceptation pleine et entière des présentes CGU. Si un utilisateur n'accepte pas les CGU, il doit s'abstenir d'utiliser le site.
         </p>
 
-        <p>Accès au site
+        <h3>Accès au site</h3>
+        <p>
           L'accès au site est ouvert à tous les utilisateurs disposant d'une connexion internet et d'un navigateur web. L'accès peut être suspendu ou interrompu à tout moment pour des raisons techniques ou pour des raisons de maintenance.
         </p>
 
-        <p>Contenu du site
+        <h3>Contenu du site</h3>
+        <p>
           Le site permet aux artistes de publier des images de leurs œuvres. Les images sont libres de droits et peuvent être téléchargées et utilisées pour un usage personnel ou éducatif. Cependant, il est strictement interdit d'utiliser ces images à des fins commerciales sans l'autorisation écrite de l'artiste.
         </p>
 
-        <p>Responsabilité
+        <h3>Responsabilité</h3>
+        <p>
           L'administrateur du site ne peut être tenu responsable de tout dommage direct ou indirect résultant de l'utilisation du site. L'utilisateur assume l'entière responsabilité de l'utilisation du site et de son contenu.
         </p>
 
-        <p>Propriété intellectuelle
-          Le site et son contenu sont la propriété de [Ecole O'clock SAS, situé au 10 RUE DE PENTHIEVRE 75008 PARIS ]. Toute reproduction, distribution ou utilisation du site ou de son contenu sans l'autorisation écrite de [Ecole O'clock] est strictement interdite.
+        <h3>Propriété intellectuelle</h3>
+        <p>
+          Le site et son contenu sont la propriété de <strong>Ecole O'clock SAS</strong>. Toute reproduction, distribution ou utilisation du site ou de son contenu sans l'autorisation écrite de <strong>Ecole O'clock</strong> est strictement interdite.
         </p>
 
-        <p>Données personnelles
+        <h3>Données personnelles</h3>
+        <p>
           Le site collecte des données personnelles sur les utilisateurs, notamment lors de l'inscription. Les données collectées sont utilisées pour la gestion du site et pour communiquer avec les utilisateurs. Les données personnelles ne sont pas transmises à des tiers sans le consentement préalable de l'utilisateur.
         </p>
 
-        <p>Modifications des CGU
+        <h3>Modifications des CGU</h3>
+        <p>
           Les présentes CGU peuvent être modifiées à tout moment. Les utilisateurs seront informés des modifications par une notification sur le site. L'utilisation continue du site après la notification des modifications implique l'acceptation des nouvelles CGU.
         </p>
 
-        <p>Loi applicable et juridiction compétente
+        <h3>Loi applicable et juridiction compétente</h3>
+        <p>
           Les présentes CGU sont régies par le droit français. Tout litige relatif à l'utilisation du site sera soumis à la juridiction compétente.
         </p>
-      </div>
+      </section>
     </Page>
     <Footer />
   </>
