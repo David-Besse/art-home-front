e8a479fee695db4ecc9920df455b4a04a35c60a3
David-Besse <dahodprod@gmail.com>
2023-05-09 10:25:35 +0200

    changing routes api to localhost

--- src/middlewares/exhibitionsMiddleware.js
+++ src/middlewares/exhibitionsMiddleware.js
@@ -3,14 +3,13 @@ import {
   FETCH_EXHIBITIONS, saveExhibitions,
   FETCH_USER_ARTWORKS, saveUserArtworks,
   UPDATE_USER_ARTWORK, SUBMIT_NEW_ARTWORK,
-  DELETE_USER_ARTWORK, fetchUserArtworks,
+  DELETE_USER_ARTWORK,
 } from '../actions/exhibitions';
-import { showSelectedExhibition } from '../actions/profile';
 
 const exhibitionsMiddleware = (store) => (next) => (action) => {
   switch (action.type) {
     case FETCH_EXHIBITIONS:
-      axios.get('http://aurelia-perrier-server.eddi.cloud/projet-12-art-at-home-back/public/api/exhibitions/homepage')
+      axios.get('http://localhost:8000/api/exhibitions/homepage')
         .then((response) => {
           setTimeout(() => {
             store.dispatch(saveExhibitions(response.data));
@@ -22,7 +21,7 @@ const exhibitionsMiddleware = (store) => (next) => (action) => {
       break;
     case FETCH_USER_ARTWORKS:
       axios.get(
-        `http://aurelia-perrier-server.eddi.cloud/projet-12-art-at-home-back/public/api/secure/artworks/exhibitions/${action.payload}/profile`,
+        `http://localhost:8000/api/secure/artworks/exhibitions/${action.payload}/profile`,
         {
           headers: {
             Authorization: `Bearer ${store.getState().users.token}`,
@@ -42,7 +41,7 @@ const exhibitionsMiddleware = (store) => (next) => (action) => {
       break;
     case UPDATE_USER_ARTWORK:
       axios.patch(
-        `http://aurelia-perrier-server.eddi.cloud/projet-12-art-at-home-back/public/api/secure/artworks/${action.artworkId}/edit`,
+        `http://localhost:8000/api/secure/artworks/${action.artworkId}/edit`,
         {
           title: action.data.title,
           description: action.data.description,
@@ -64,7 +63,7 @@ const exhibitionsMiddleware = (store) => (next) => (action) => {
       break;
     case SUBMIT_NEW_ARTWORK:
       axios.post(
-        'http://aurelia-perrier-server.eddi.cloud/projet-12-art-at-home-back/public/api/secure/artworks/new',
+        'http://localhost:8000/api/secure/artworks/new',
         {
           title: action.payload.title,
           description: action.payload.description,
@@ -86,7 +85,7 @@ const exhibitionsMiddleware = (store) => (next) => (action) => {
       break;
     case DELETE_USER_ARTWORK:
       axios.delete(
-        `http://aurelia-perrier-server.eddi.cloud/projet-12-art-at-home-back/public/api/secure/artworks/${action.id}/delete`,
+        `http://localhost:8000/api/secure/artworks/${action.id}/delete`,
         {
           headers: {
             Authorization: `Bearer ${store.getState().users.token}`,
