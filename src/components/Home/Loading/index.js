de59e53d87a61f359a9fb4670f0aa7eb10e3b428
David-Besse <dahodprod@gmail.com>
2023-05-11 21:28:06 +0200

    fix wrong position footer when loader is activated

--- src/components/Home/Loading/index.js
+++ src/components/Home/Loading/index.js
@@ -2,7 +2,7 @@ import './style.scss';
 
 // loader page
 const Loading = () => (
-  <section>
+  <section className="loading-section">
     <p className="loading">Nos peintures sèchent ...</p>
   </section>
 );
