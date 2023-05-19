beaff26b4d1d2147385b4892a096191166cb620f
David-Besse <dahodprod@gmail.com>
2023-05-19 18:33:40 +0200

    CSS rework in exhibitionsManager, better

--- src/components/Profile/ExhibitionsManager/index.js
+++ src/components/Profile/ExhibitionsManager/index.js
@@ -110,7 +110,7 @@ const ExhibitionsManager = () => {
   };
 
   return (
-    <section className="exhibitionManager">
+    <section className="exhibitionManager mt-3">
 
       {/* Modal for creating an exhibition */}
       <Modal
@@ -228,9 +228,11 @@ const ExhibitionsManager = () => {
 
       {/* Show exhibition management */}
       <div className="allExhibitions justify-content-center">
-        <div className="mt-3 mb-3">
-          <div className="d-flex justify-content-between">
+        <div className="mt-3 mb-3 border-top">
+          <div className="">
             <h3 className="mb-3 my-3 fw-bolder">Gestionnaire d'expositions</h3>
+          </div>
+          <div className="d-flex flex-raw justify-content-between p-1">
             <div className="d-flex">
 
               {/* buttons to manage the modal for creating an exhibition */}
@@ -253,38 +255,35 @@ const ExhibitionsManager = () => {
 
               {/* button to manage the modal for adding an artwork */}
               {exhibitions.length > 0 && (
-                <>
-                  <Button
-                    variant="primary"
-                    onClick={handleArtworkCreationModal}
-                    className="my-3 d-none d-sm-block customButton"
-                  >
-                    Ajouter une oeuvre
-                  </Button>
-                  <Button
-                    variant="primary"
-                    onClick={handleArtworkCreationModal}
-                    className="my-3 d-block d-sm-none customButton"
-                  >
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-brush" viewBox="0 0 16 16">
-                      <path d="M15.825.12a.5.5 0 0 1 .132.584c-1.53 3.43-4.743 8.17-7.095 10.64a6.067 6.067 0 0 1-2.373 1.534c-.018.227-.06.538-.16.868-.201.659-.667 1.479-1.708 1.74a8.118 8.118 0 0 1-3.078.132 3.659 3.659 0 0 1-.562-.135 1.382 1.382 0 0 1-.466-.247.714.714 0 0 1-.204-.288.622.622 0 0 1 .004-.443c.095-.245.316-.38.461-.452.394-.197.625-.453.867-.826.095-.144.184-.297.287-.472l.117-.198c.151-.255.326-.54.546-.848.528-.739 1.201-.925 1.746-.896.126.007.243.025.348.048.062-.172.142-.38.238-.608.261-.619.658-1.419 1.187-2.069 2.176-2.67 6.18-6.206 9.117-8.104a.5.5 0 0 1 .596.04zM4.705 11.912a1.23 1.23 0 0 0-.419-.1c-.246-.013-.573.05-.879.479-.197.275-.355.532-.5.777l-.105.177c-.106.181-.213.362-.32.528a3.39 3.39 0 0 1-.76.861c.69.112 1.736.111 2.657-.12.559-.139.843-.569.993-1.06a3.122 3.122 0 0 0 .126-.75l-.793-.792zm1.44.026c.12-.04.277-.1.458-.183a5.068 5.068 0 0 0 1.535-1.1c1.9-1.996 4.412-5.57 6.052-8.631-2.59 1.927-5.566 4.66-7.302 6.792-.442.543-.795 1.243-1.042 1.826-.121.288-.214.54-.275.72v.001l.575.575zm-4.973 3.04.007-.005a.031.031 0 0 1-.007.004zm3.582-3.043.002.001h-.002z" />
-                    </svg>
-                  </Button>
-                </>
+              <>
+                <Button
+                  variant="primary"
+                  onClick={handleArtworkCreationModal}
+                  className="my-3 d-none d-sm-block customButton"
+                >
+                  Ajouter une oeuvre
+                </Button>
+                <Button
+                  variant="primary"
+                  onClick={handleArtworkCreationModal}
+                  className="my-3 d-block d-sm-none customButton"
+                >
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-brush" viewBox="0 0 16 16">
+                    <path d="M15.825.12a.5.5 0 0 1 .132.584c-1.53 3.43-4.743 8.17-7.095 10.64a6.067 6.067 0 0 1-2.373 1.534c-.018.227-.06.538-.16.868-.201.659-.667 1.479-1.708 1.74a8.118 8.118 0 0 1-3.078.132 3.659 3.659 0 0 1-.562-.135 1.382 1.382 0 0 1-.466-.247.714.714 0 0 1-.204-.288.622.622 0 0 1 .004-.443c.095-.245.316-.38.461-.452.394-.197.625-.453.867-.826.095-.144.184-.297.287-.472l.117-.198c.151-.255.326-.54.546-.848.528-.739 1.201-.925 1.746-.896.126.007.243.025.348.048.062-.172.142-.38.238-.608.261-.619.658-1.419 1.187-2.069 2.176-2.67 6.18-6.206 9.117-8.104a.5.5 0 0 1 .596.04zM4.705 11.912a1.23 1.23 0 0 0-.419-.1c-.246-.013-.573.05-.879.479-.197.275-.355.532-.5.777l-.105.177c-.106.181-.213.362-.32.528a3.39 3.39 0 0 1-.76.861c.69.112 1.736.111 2.657-.12.559-.139.843-.569.993-1.06a3.122 3.122 0 0 0 .126-.75l-.793-.792zm1.44.026c.12-.04.277-.1.458-.183a5.068 5.068 0 0 0 1.535-1.1c1.9-1.996 4.412-5.57 6.052-8.631-2.59 1.927-5.566 4.66-7.302 6.792-.442.543-.795 1.243-1.042 1.826-.121.288-.214.54-.275.72v.001l.575.575zm-4.973 3.04.007-.005a.031.031 0 0 1-.007.004zm3.582-3.043.002.001h-.002z" />
+                  </svg>
+                </Button>
+              </>
               )}
 
             </div>
-          </div>
-          <div className="d-flex flex-raw mb-3">
-
             {/* button to select an exhibition */}
-            <DropdownButton
-              className="d-flex"
-              variant="secondary"
-              id="selectExhibition"
-              title={(
-                <>
-                  {isArtworksLoading
+            <div className="d-flex align-items-end">
+              <DropdownButton
+                className="selectExhibition"
+                variant="secondary"
+                title={(
+                  <>
+                    {isArtworksLoading
                   && (
                     <Spinner
                       as="span"
@@ -295,15 +294,14 @@ const ExhibitionsManager = () => {
                       className="me-2"
                     />
                   )}
-                  {(exhibitions.length > 0
-                    ? ' Voir une exposition'
-                    : ' Aucune exposition trouvée')}
-                </>
+                    {(exhibitions.length > 0
+                      ? ' Voir une exposition'
+                      : ' Aucune exposition trouvée')}
+                  </>
               )}
-              disabled={exhibitions.length === 0}
-              size="sm"
-            >
-              {exhibitions.length > 0
+                disabled={exhibitions.length === 0}
+              >
+                {exhibitions.length > 0
                 && exhibitions.map((exhibition) => (
                   <Dropdown.Item
                     as="button"
@@ -314,7 +312,8 @@ const ExhibitionsManager = () => {
                     {exhibition.title}
                   </Dropdown.Item>
                 ))}
-            </DropdownButton>
+              </DropdownButton>
+            </div>
 
           </div>
 
--- src/components/Profile/MyFavorites/index.js
+++ src/components/Profile/MyFavorites/index.js
@@ -3,7 +3,7 @@ import './styles.scss';
 const MyFavorites = () => (
   <section className="myfavoritesSection">
     <div className="myfavoritesContainer mt-3">
-      <h2 className="myfavoritesTitle fw-bolder mt-3">Mes Favoris</h2>
+      <h3 className="myfavoritesTitle fw-bolder mt-3">Mes Favoris</h3>
     </div>
   </section>
 );
