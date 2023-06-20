e1889d74ba08a6e5d00213975306a4dc5e591a8d
David-Besse <dahodprod@gmail.com>
2023-06-20 14:00:41 +0200

    scroll to top after clicking an exhibition card

--- src/components/Exhibitions/ExhibitionsList/index.js
+++ src/components/Exhibitions/ExhibitionsList/index.js
@@ -1,13 +1,32 @@
 import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
 
 import Card from 'react-bootstrap/Card';
 
 import './style.scss';
 import { Link } from 'react-router-dom';
 
+
 // exhibitions page
 const ExhibitionsList = () => {
   const { list } = useSelector((state) => state.exhibitions);
+
+  useEffect(() => {
+    const handleCardClick = () => {
+      window.scrollTo(0, 0);
+    };
+
+    const cards = document.getElementsByClassName('cardLink');
+    Array.from(cards).forEach((card) => {
+      card.addEventListener('click', handleCardClick);
+    });
+
+    return () => {
+      Array.from(cards).forEach((card) => {
+        card.removeEventListener('click', handleCardClick);
+      });
+    };
+  }, []);
 
   return (
     <div className="all-exhibitions">
@@ -19,7 +38,7 @@ const ExhibitionsList = () => {
         {list.map((exhibition) => (
           exhibition.artwork.length !== 0
           && (
-          <Link to={`/expositions/${exhibition.slug}`} key={exhibition.slug} className="text-decoration-none">
+          <Link to={`/expositions/${exhibition.slug}`} key={exhibition.slug} className="cardLink text-decoration-none">
             <Card className="text-white card-info">
               <Card.Img className="image-info" src={exhibition.artwork[0].picture} alt={exhibition.artwork[0].slug} />
               <Card.Title className="text-center title-info">{exhibition.title}</Card.Title>
