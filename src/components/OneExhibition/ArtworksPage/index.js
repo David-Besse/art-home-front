a8ba48e591b8ff6654f55bbeeed63e6dbc8c3c70
David-Besse <dahodprod@gmail.com>
2023-07-14 16:11:03 +0200

    fix exhibition page where a picture should not be displayed related to the status

--- src/components/OneExhibition/ArtworksPage/index.js
+++ src/components/OneExhibition/ArtworksPage/index.js
@@ -74,20 +74,22 @@ const ArtworksPage = () => {
 
       {/* Showcase of all the picture included in the exhibiton */}
       <section className="picture-list">
-        {artwork.map((picture) => (
-          <Card className="card-picture" key={picture.slug}>
-            <Card.Img
-              className="image-picture"
-              src={picture.picture}
-              alt={picture.slug}
-              onClick={() => OpenModalImg(picture)}
-            />
-            <span className="iconZoom" />
-            <div
-              className="heart-icon"
-              onClick={() => handleFavorites(picture.id)}
-            >
-              {logged && !favorites.includes(picture.id) && (
+        {artwork
+          .filter((el) => el.status === true)
+          .map((el) => (
+            <Card className="card-picture" key={el.slug}>
+              <Card.Img
+                className="image-picture"
+                src={el.picture}
+                alt={el.slug}
+                onClick={() => OpenModalImg(el)}
+              />
+              <span className="iconZoom" />
+              <div
+                className="heart-icon"
+                onClick={() => handleFavorites(el.id)}
+              >
+                {logged && !favorites.includes(el.id) && (
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="16"
@@ -98,8 +100,8 @@ const ArtworksPage = () => {
                 >
                   <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z" />
                 </svg>
-              )}
-              {logged && favorites.includes(picture.id) && (
+                )}
+                {logged && favorites.includes(el.id) && (
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="16"
@@ -113,16 +115,16 @@ const ArtworksPage = () => {
                     d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"
                   />
                 </svg>
-              )}
-            </div>
-            <Card.Body className="body-picture px-0">
-              <Card.Title className="title-picture">{picture.title}</Card.Title>
-              <Card.Text className="description-picture">
-                {picture.description}
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        ))}
+                )}
+              </div>
+              <Card.Body className="body-picture px-0">
+                <Card.Title className="title-picture">{el.title}</Card.Title>
+                <Card.Text className="description-picture">
+                  {el.description}
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          ))}
       </section>
 
       <ModalImage />
